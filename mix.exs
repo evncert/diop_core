993a44846d76d044f6635c0ced06fe08aaf592e8
Sona Tigranyan <sonatigranyan1998@gmail.com>
2020-05-02 23:48:41 +0400

    added distillery

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DiopCore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger,],
+      extra_applications: [:logger, :mnesia],
       mod: {DiopCore.Application, []}
     ]
   end
@@ -25,6 +25,7 @@ defmodule DiopCore.MixProject do
       {:mime, "~> 1.3"},
       {:diop_email, path: "apps/diop_email"},
       {:diop_csv, path: "apps/diop_csv"},
+      {:distillery, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
